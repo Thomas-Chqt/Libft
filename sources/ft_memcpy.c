16fbee61528a073ecab3055ae95b59b4e17b51e8
Thomas <tchoquet@student.42tokyo.jp>
2023-05-21 13:59:08 +0900

    fix memecpy

--- sources/ft_memcpy.c
+++ sources/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 13:17:27 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/05/21 13:21:32 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/05/21 13:57:10 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (dst == NULL || src == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
