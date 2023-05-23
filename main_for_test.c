929da20b71ff42d987b91b0eb621481f2d5856ca
Thomas <tchoquet@student.42tokyo.jp>
2023-05-23 16:15:11 +0900

    fix memcpy

--- main_for_test.c
+++ main_for_test.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:25:16 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/05/22 17:45:14 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/05/23 16:13:25 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 
 int main()
 {
-	char *s = "libft-test-tokyo";
-	char *res = ft_strrchr(s, 's');
-	char *res2 = strrchr(s, 's');
+	unsigned char src[5] = {0xAA, 0xBB, 0xCC} ;
+	unsigned char des[5] = {0x00, 0x00, 0x00} ;
+
+	unsigned char *s = ft_memcpy(NULL, NULL, 2);
 
 	return 0; 
 }
--- sources/ft_memcpy.c
+++ sources/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 13:17:27 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/05/21 13:57:10 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/05/23 16:13:44 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (dst == NULL || src == NULL)
+	if (dst == NULL)
 		return (NULL);
 	while (i < n)
 	{
