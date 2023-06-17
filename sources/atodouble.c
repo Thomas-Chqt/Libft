6cbe80f58fa3771fe828cdb302d010cb7eb780b9
Thomas <semoir.dense-0h@icloud.com>
2023-06-17 10:38:56 +0900

    feat: create atodouble.c

---
+++ sources/atodouble.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atodouble.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 01:25:57 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/06/17 01:27:24 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned short	intlen(int n);
+
+double	atodouble(const char *str)
+{
+	int				entier;
+	double			virgule;
+	unsigned short	v_len;
+
+	entier = (double)atoi(str);
+	virgule = (double)atoi(ft_strchr(str, '.') + 1);
+	v_len = intlen((int)virgule);
+	while (v_len > 0)
+	{
+		virgule /= (double)10;
+		v_len--;
+	}
+	if (entier > 0)
+		return ((double)entier + virgule);
+	return ((double)entier - virgule);
+}
+
+static unsigned short	intlen(int n)
+{
+	unsigned int	nbr;
+	unsigned short	len;
+
+	len = 1;
+	if (n < 0)
+	{
+		len++;
+		nbr = n * -1;
+	}
+	else
+		nbr = n;
+	while ((nbr / 10) > 0)
+	{
+		len++;
+		nbr /= 10;
+	}
+	return (len);
+}
