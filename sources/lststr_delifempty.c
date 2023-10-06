83020b87a1c48abcc2060f0701dcf7a9f30ff375
Thomas <tchoquet@student.42tokyo.jp>
2023-10-06 14:11:44 +0900

    fix compile error

--- sources/lststr_delifempty.c
+++ sources/lststr_delifempty.c
@@ -6,20 +6,20 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 12:20:36 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/07/09 12:24:25 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/10/06 14:04:33 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_internal.h"
 
-static t_bool	is_empty_str(void *str_ptr, void *none);
+static t_bool	stat_is_empty_str(void *str_ptr, void *none);
 
 void	lststr_delifempty(t_list **head)
 {
-	lst_delif(head, &free_str_ptr, &is_empty_str, NULL);
+	lst_delif(head, &free_str_ptr, &stat_is_empty_str, NULL);
 }
 
-static t_bool	is_empty_str(void *str_ptr, void *none)
+static t_bool	stat_is_empty_str(void *str_ptr, void *none)
 {
 	char	*str;
 
