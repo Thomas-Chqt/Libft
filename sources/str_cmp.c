10a12b57048497af1e230cde7697544b0cfcde3e
Thomas <tchoquet@student.42tokyo.jp>
2023-10-22 19:41:30 +0900

    str_cmp no crash

--- sources/str_cmp.c
+++ sources/str_cmp.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 20:39:06 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/07/05 20:41:52 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/10/22 13:08:05 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ int	str_cmp(const char *s1, const char *s2)
 {
 	size_t	i;
 
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	if (s1 == NULL && s2 != NULL)
+		return (-1);
+	if (s1 != NULL && s2 == NULL)
+		return (1);
 	i = 0;
 	while (s1[i] && s2[i])
 	{
